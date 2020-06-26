87876fbba8405266278306d1082438c498e73148
Patrick Gatewood <pg8wood@vt.edu>
2020-06-26 13:30:31 -0400

    Create app using new multiplatform app template

---
+++ wwdc-2020/Shared/wwdc_2020App.swift
@@ -0,0 +1,23 @@
+//
+//  wwdc_2020App.swift
+//  Shared
+//
+//  Created by Patrick Gatewood on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct wwdc_2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct wwdc_2020App_Previews: PreviewProvider {
+    static var previews: some View {
+       ContentView()
+    }
+}
