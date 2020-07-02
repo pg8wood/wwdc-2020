3ba533e719b9b86a1db8401540bd55e69c8717c6
Patrick Gatewood <pg8wood@vt.edu>
2020-07-02 13:14:33 -0400

    Add documentation for AnyProgressViewStyle and why not to use it again

--- wwdc-2020/Shared/ProgressViewExampleView.swift
+++ wwdc-2020/Shared/ProgressViewExampleView.swift
@@ -4,9 +4,14 @@
 //
 //  Created by Patrick Gatewood on 6/30/20.
 //
+// An attempt at creating my own type erasure similar to `AnyView`. Not recommended
+// unless we figure out a way to restore `AnyProgressviewStyle`'s wrapped view's type.
+// See:https://forums.swift.org/t/why-some-swiftui-views-have-body-swift-never/27372/8
 
 import SwiftUI
 import Combine
+
+// MARK: - Type Erasure
 
 protocol TypeErasing {
     var erasedValue: Any { get }
@@ -19,26 +24,27 @@ struct ProgressViewStyleTypeEraser<S: ProgressViewStyle>: TypeErasing {
     }
 }
 
+// MARK: - Type-erased `ProgressViewStyle` attempt
+
+/// After some research, it seems this isn't possible yet without leveraging SwiftUI's internal implementations.
+/// See: https://forums.swift.org/t/why-some-swiftui-views-have-body-swift-never/27372/8
 struct AnyProgressViewStyle: ProgressViewStyle {
-    private var innerStyle: some ProgressViewStyle = DefaultProgressViewStyle()
-    
     private var eraser: TypeErasing
-    public init<S>(_ style: S) where S : ProgressViewStyle {
+    
+    init<S>(_ style: S) where S : ProgressViewStyle {
         eraser = ProgressViewStyleTypeEraser(style: style)
     }
     
     private var wrappedStyle: Any {
         eraser.erasedValue
     }
-    
-    // MARK: - ProgressViewStyle
-    
+        
+    /// In AnyView, this type is Never, telling SwiftUI to internally call its wrapped subview's `body` instead.
     typealias Body = AnyView
     
     func makeBody(configuration: Self.Configuration) -> Self.Body {
-        // TODO: Figure out how AnyView works with respect to reconstituting the type of View it wraps and mirros
-        // that functionality here instead of casting
-        
+        // See the AnyProgressViewStyle documentation for why we have to do this.
+        // Overall, this is not a pattern I'd recommend using moving forward.
         if let style = wrappedStyle as? DefaultProgressViewStyle {
             return AnyView(style.makeBody(configuration: configuration))
         } else if let style = wrappedStyle as? CircularProgressViewStyle {
@@ -46,21 +52,25 @@ struct AnyProgressViewStyle: ProgressViewStyle {
         } else if let style = wrappedStyle as? LinearProgressViewStyle {
             return AnyView(style.makeBody(configuration: configuration))
         }
-        
+
         fatalError("Need to figure out to to restore the type of ProgressViewStyle!")
     }
 }
 
+// MARK: - The example view which made me want to attempt to make my own type erasure
+
 struct ProgressViewExampleView: View {
     @State private var cancellables = Set<AnyCancellable>()
-    @State private var currentProgressViewStyle: ProgressStyle = .default
     @State private var downloadAmount = 0.0
+    
+    // The offender. Since `ProgressViewStyle` has associated type constraints, we
+    // can't store a state variable of type `ProgressViewStyle`
+    @State private var currentProgressViewStyle: ProgressStyle = .default
     
     enum ProgressStyle: String, CaseIterable {
         case `default` = "Default"
         case linear = "Linear"
         case circular = "Circular"
-        // TODO: custom style
         
         var body: some ProgressViewStyle {
             switch self {
@@ -73,8 +83,6 @@ struct ProgressViewExampleView: View {
             }
         }
     }
-    
-    
 
     var body: some View {
         VStack(alignment: .leading) {
