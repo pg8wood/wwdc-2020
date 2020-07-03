78011028cf4c94c453e1be01feb1c612e7eceb6b
Patrick Gatewood <pg8wood@vt.edu>
2020-07-03 18:26:12 -0400

    Add images to custom Progressview

--- wwdc-2020/Shared/Progress Views/LoadingMessageProgressView.swift
+++ wwdc-2020/Shared/Progress Views/LoadingMessageProgressView.swift
@@ -14,21 +14,56 @@ struct LoadingMessageProgressView: View {
     
     private var messages = [
         "Loading...",
-        "Still loading...",
-        "STILL loading...",
         "Is anybody there?",
         "I'm so lonely"
+    ]
+    
+    private var imageNames = [
+        "kermit-thinking",
+        "kermit-fishing",
+        "kermit-rain"
     ]
     
     private var currentMessage: String {
         messages[messageIndex % messages.count]
     }
     
+    
+    private var loadingMessages: [AnyView] = {
+        func loadingImage(_ name: String) -> some View {
+            Image(name)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: .continuous))
+        }
+        
+        func loadingText(_ text: String ) -> some View {
+            Text(text)
+                .frame(width: 200, height: 200, alignment: .top)
+        }
+        
+        return [
+            loadingText("Loading..."),
+            loadingText("Is anybody there?"),
+            loadingText("I'm some lonely"),
+            loadingImage("kermit-thinking"),
+            loadingImage("kermit-fishing"),
+            loadingImage("kermit-rain")
+        ]
+        .compactMap { AnyView(_fromValue: $0) }
+    }()
+    
+    
+    private var currentLoadingMessage: some View {
+        loadingMessages[messageIndex % loadingMessages.count]
+    }
+    
     var body: some View {
         ProgressView {
-            Text(currentMessage)
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .id("LoadingMessageViewText: \(currentMessage)")
+            currentLoadingMessage
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .top)
+                .id("LoadingMessageViewText: \(currentMessage)") // will tell SwiftUI to use the transition even if the view is changing to a view of the same type (i.e. Image --> Image)
                 .transition(.slide)
                 .animation(.default)
         }
